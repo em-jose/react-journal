d7089e16e8f81edf72fb51e1041c1f056bed042e
Jose Escribano <jescribanomora@gmail.com>
2024-07-09 14:45:07 +0200

    Configure aliases

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,7 @@ module.exports = {
     testEnvironment: "jest-environment-jsdom",
     setupFiles: ["./jest.setup.js"],
     transformIgnorePatterns: [],
+    moduleNameMapper: {
+        "^@/(.*)$": "<rootDir>/src/$1",
+    },
 };
