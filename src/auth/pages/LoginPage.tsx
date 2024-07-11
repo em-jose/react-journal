c6f68931868ce6a8f6c754a8442eac95a9d8eda4
Jose Escribano <jescribanomora@gmail.com>
2024-07-11 11:36:53 +0200

    Add login page buttons

--- src/auth/pages/LoginPage.tsx
+++ src/auth/pages/LoginPage.tsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 
 export const LoginPage = () => {
     return (
@@ -41,6 +43,33 @@ export const LoginPage = () => {
                                 fullWidth
                             />
                         </Grid>
+
+                        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                            <Grid item xs={12} sm={6}>
+                                <Button variant="contained" fullWidth>
+                                    Login
+                                </Button>
+                            </Grid>
+
+                            <Grid item xs={12} sm={6}>
+                                <Button variant="contained" fullWidth>
+                                    <Google />
+                                    <Typography sx={{ ml: 1 }}>
+                                        Google
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container direction="row" justifyContent="end">
+                        <Link
+                            component={RouterLink}
+                            color="inherit"
+                            to="/auth/register"
+                        >
+                            Create account
+                        </Link>
                     </Grid>
                 </form>
             </Grid>
