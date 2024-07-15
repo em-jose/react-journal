408598201e5be74b547613c30a6043305efb4313
Jose Escribano <jescribanomora@gmail.com>
2024-07-15 18:07:35 +0200

    Add navbar to journal layout

--- src/journal/layout/JournalLayout.tsx
+++ src/journal/layout/JournalLayout.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
+import { Navbar } from "@journal/components/";
 
 const DRAWER_WIDTH: number = 240;
 
 export const JournalLayout: React.FC<Props> = ({ children }) => {
     return (
         <Box sx={{ display: "flex" }}>
+            <Navbar drawerWidth={DRAWER_WIDTH} />
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 {children}
             </Box>
