8f03ec0867e273b93a87ceaf2a6e4d4625b9807c
Jose Escribano <jescribanomora@gmail.com>
2024-07-22 16:16:19 +0200

    Create authentication basic thunks

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,1 +1,2 @@
 export * from "@/store/auth/authSlice";
+export * from "@/store/auth/thunks";
---
+++ src/store/auth/thunks.ts
@@ -0,0 +1,13 @@
+import { checkingCredentials } from "@/store/auth";
+
+export const checkingAuthentication = (email: string, password: string) => {
+    return async (dispatch) => {
+        dispatch(checkingCredentials());
+    };
+};
+
+export const startGoogleSignIn = () => {
+    return async (dispatch) => {
+        dispatch(checkingCredentials());
+    };
+};
