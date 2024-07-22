123924992bc4e64db5377c72f4bfb451edbeb9ff
Jose Escribano <jescribanomora@gmail.com>
2024-07-22 16:43:32 +0200

    Firebase initial config

---
+++ src/firebase/config.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
+import { getEnvironments } from "@/helpers/getEnvironments";
+
+const {
+    VITE_FIREBASE_API_KEY,
+    VITE_FIREBASE_AUTH_DOMAIN,
+    VITE_FIREBASE_PROJECT_ID,
+    VITE_FIREBASE_STORAGE_BUCKET,
+    VITE_FIREBASE_MESSAGE_IN_SENDER_ID,
+    VITE_FIREBASE_APP_ID,
+} = getEnvironments();
+
+const firebaseConfig = {
+    apiKey: VITE_FIREBASE_API_KEY,
+    authDomain: VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: VITE_FIREBASE_PROJECT_ID,
+    storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: VITE_FIREBASE_MESSAGE_IN_SENDER_ID,
+    appId: VITE_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig);
+export const FirebaseAuth = getAuth(FirebaseApp);
+export const FirebaseDB = getFirestore(FirebaseApp);
