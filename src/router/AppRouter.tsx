d3a45e9b34b2cdb59f980a9b7d2f679e1d6451a9
Jose Escribano <emjose.dev@gmail.com>
2024-07-23 10:11:36 +0200

    Check if user is authenticated

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "@/firebase/config";
 import { AuthRoutes } from "@/auth/routes/AuthRoutes";
 import { JournalRoutes } from "@journal/routes/JournalRoutes";
+import { login, logout } from "@/store/auth";
 import { CheckingAuth } from "@/ui";
 
 export const AppRouter = () => {
     const { status } = useSelector((state) => state.auth);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        onAuthStateChanged(FirebaseAuth, async (user) => {
+            if (!user) return dispatch(logout());
+
+            const { uid, email, displayName, photoURL } = user;
+            dispatch(login({ uid, email, displayName, photoURL }));
+        });
+    }, []);
 
     if (status === "checking") {
         return <CheckingAuth />;
@@ -13,8 +27,13 @@ export const AppRouter = () => {
 
     return (
         <Routes>
-            <Route path="/auth/*" element={<AuthRoutes />} />
-            <Route path="/*" element={<JournalRoutes />} />
+            {status === "authenticated" ? (
+                <Route path="/*" element={<JournalRoutes />} />
+            ) : (
+                <Route path="/auth/*" element={<AuthRoutes />} />
+            )}
+
+            <Route path="/*" element={<Navigate to="/auth/login" />} />
         </Routes>
     );
 };
